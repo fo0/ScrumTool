6d506133fb39f393df50e9d55e0c487880dd2eff
fo0 <fo0@fo0.me>
2020-05-20 12:08:44 -0700

    refactor

--- ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/config/ProjectBoardConfig.java
+++ ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/config/ProjectBoardConfig.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class ProjectBoardConfig {
 
-	@Value("app.projectboard.columns.max: 3")
-	public static int MAX_COLUMNS = 5;
+	@Value("${app.projectboard.columns.max: 3}")
+	public static int MAX_COLUMNS;
 
-	@Value("app.projectboard.cards.max: 5")
-	public static int MAX_CARDS = 50;
+	@Value("${app.projectboard.cards.max: 5}")
+	public static int MAX_CARDS;
 
-	@Value("app.projectboard.cards.text.length.max: 10")
+	@Value("${app.projectboard.cards.text.length.max: 50}")
 	public static int MAX_CARD_TEXT_LENGTH;
 
 }
