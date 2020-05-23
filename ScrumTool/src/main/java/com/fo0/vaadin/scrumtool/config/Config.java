a7d5b5b8b0fe0c356873125939057765ac8779c6
fo0 <fo0@fo0.me>
2020-05-23 11:26:15 -0700

    added debug mode to config

--- ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/config/Config.java
+++ ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/config/Config.java
@@ -1,9 +1,15 @@
 package com.fo0.vaadin.scrumtool.config;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Config {
 
-	public static final boolean DEBUG = true;
-	
+	public static boolean DEBUG = true;
+
 	public static int NOTIFICATION_DURATION = 2000;
 
+	@Value("${app.debug: false}")
+	public void setMAX_COLUMNS(boolean DEBUG) {
+		Config.DEBUG = DEBUG;
+	}
 }
