3d5e5eb716b9c35b6de976342e4f92a2d5c1f271
KaesDingeling <kingdingeling721@gmail.com>
2020-05-31 22:09:09 +0200

    remove unused theme paramerts for textfields

--- ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/views/dialogs/CreateBoardDialog.java
+++ ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/views/dialogs/CreateBoardDialog.java
@@ -18,7 +18,6 @@ import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
-import com.vaadin.flow.component.textfield.TextFieldVariant;
 
 public class CreateBoardDialog extends Dialog {
 
