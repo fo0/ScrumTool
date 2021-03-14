503d8c65f70dc17b8d70ffe1fb37d2cb249f40df
fo0 <fo0@fo0.me>
2021-03-14 17:28:15 +0000

    added pem cert

--- ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/ui/Main.java
+++ ScrumTool/src/main/java/com/fo0/vaadin/scrumtool/ui/Main.java
@@ -1,10 +1,14 @@
 package com.fo0.vaadin.scrumtool.ui;
 
+import java.security.Security;
+
+import com.fo0.vaadin.scrumtool.ui.config.Profiles;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import com.fo0.vaadin.scrumtool.ui.config.Profiles;
+import de.dentrassi.crypto.pem.PemKeyStoreProvider;
 
 /**
  * The entry point of the Spring Boot application.
@@ -13,6 +17,7 @@ import com.fo0.vaadin.scrumtool.ui.config.Profiles;
 public class Main extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+		Security.addProvider(new PemKeyStoreProvider());
 		//@formatter:off
 		new SpringApplicationBuilder(Main.class)
 			.properties("vaadin.heartbeatinterval=5")
